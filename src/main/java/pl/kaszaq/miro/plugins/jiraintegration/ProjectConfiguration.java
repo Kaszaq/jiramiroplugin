667f25a12053951e963471cefcf301208dc002a6
Kaszaq <kaszaq@gmail.com>
2019-10-22 01:25:01 +0200

    * updated spring boot to 2.2.0 which upgraded spring to 5.2 hence changed spring security * added ability to refresh access tokens

--- src/main/java/pl/kaszaq/miro/plugins/jiraintegration/ProjectConfiguration.java
+++ src/main/java/pl/kaszaq/miro/plugins/jiraintegration/ProjectConfiguration.java
@@ -5,6 +5,12 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
@@ -19,5 +25,24 @@ public class ProjectConfiguration {
         filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
         return filterRegistrationBean;
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        OAuth2AuthorizedClientProvider authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .authorizationCode()
+                        .refreshToken()
+                        .build();
+
+        DefaultOAuth2AuthorizedClientManager authorizedClientManager =
+                new DefaultOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
     }
 }
--- src/main/java/pl/kaszaq/miro/plugins/jiraintegration/controllers/AccessTokenController.java
+++ src/main/java/pl/kaszaq/miro/plugins/jiraintegration/controllers/AccessTokenController.java
@@ -2,8 +2,11 @@ package pl.kaszaq.miro.plugins.jiraintegration.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.endpoint.DefaultRefreshTokenTokenResponseClient;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.*;
@@ -16,16 +19,26 @@ public class AccessTokenController {
     @Autowired
     private OAuth2AuthorizedClientService authorizedClientService;
 
-//    @CrossOrigin(origins = "*")
+    @Autowired
+    private OAuth2AuthorizedClientManager manager;
+
+
     @RequestMapping(method = RequestMethod.GET, value = "/getAccessToken")
     public String transitionIssueController(OAuth2AuthenticationToken authentication) {
         if (authentication == null) {
             return null;
         }
+
+
         OAuth2AuthorizedClient authorizedClient =
                 this.authorizedClientService.loadAuthorizedClient(
                         authentication.getAuthorizedClientRegistrationId(),
                         authentication.getName());
+
+
+        OAuth2AuthorizeRequest request = OAuth2AuthorizeRequest.withAuthorizedClient(authorizedClient).principal(authentication).build();
+        authorizedClient = manager.authorize(request);
+
 
         OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
         return accessToken.getTokenValue();
